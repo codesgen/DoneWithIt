8a34d704d22da96d3fe04aa35b1e3c9f6a4e13e4
Abel Masila <abelmasila@gmail.com>
2021-04-08 02:27:05 +0300

    learnt Views, Text and Image components

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Todo!</Text>
+      <Text numberOfLines={1} onPress={() => console.log("Text clicked")}>
+        In the following example, the nested title and body text will inherit
+        the fontFamily from styles.baseText, but the title provides its own
+        additional styles. The title and body will stack on top of each other on
+        account of the literal newlines:
+      </Text>
+      <Image
+        source={{
+          uri: "https://picsum.photos/200/300",
+          width: 200,
+          height: 300,
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +27,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
   },
 });
