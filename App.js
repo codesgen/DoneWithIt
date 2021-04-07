26edfb721d22ad2ff20a8c2645bdf51a9469efcd
Abel Masila <abelmasila@gmail.com>
2021-04-08 02:34:08 +0300

    button

--- App.js
+++ App.js
@@ -1,29 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableHighlight,
-  TouchableNativeFeedback,
-} from "react-native";
+import { Button, StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text numberOfLines={1} onPress={() => console.log("Text clicked")}>
-        In the following example, the nested title and body text will inherit
-        the fontFamily from styles.baseText, but the title provides its own
-        additional styles. The title and body will stack on top of each other on
-        account of the literal newlines:
-      </Text>
-      <TouchableNativeFeedback onPress={() => console.log("Image Touched")}>
-        <View
-          style={{ width: 200, height: 70, backgroundColor: "blue" }}
-        ></View>
-      </TouchableNativeFeedback>
-      <StatusBar style="auto" />
+      <Button
+        color="orange"
+        title="Click me"
+        onPress={() => console.log("button clicked")}
+      ></Button>
     </View>
   );
 }
