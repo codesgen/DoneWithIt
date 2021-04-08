9132177e7c56bf9190416f64dad53abcec6f5094
Abel Masila <abelmasila@gmail.com>
2021-04-09 02:33:17 +0300

    flex 102

--- App.js
+++ App.js
@@ -3,10 +3,22 @@ import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: "#fff", flex: 1 }}>
-      <View style={{ backgroundColor: "dodgerblue", flex: 2 }}></View>
-      <View style={{ backgroundColor: "gold", flex: 1 }}></View>
-      <View style={{ backgroundColor: "tomato", flex: 1 }}></View>
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}
+      ></View>
+      <View style={{ backgroundColor: "gold", width: 100, height: 100 }}></View>
+      <View
+        style={{ backgroundColor: "tomato", width: 100, height: 100 }}
+      ></View>
     </View>
   );
 }
