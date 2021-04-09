bf387b7c95923a770b4cd4217fb85334d63e3a64
Abel Masila <abelmasila@gmail.com>
2021-04-09 03:05:16 +0300

    Added Welcome screen

--- App.js
+++ App.js
@@ -1,33 +1,6 @@
 import React from "react";
-import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <View
-        style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}
-      ></View>
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-          top: 20,
-          left: 20,
-          position: "absolute",
-        }}
-      ></View>
-      <View
-        style={{ backgroundColor: "tomato", width: 100, height: 100 }}
-      ></View>
-    </View>
-  );
+  return <WelcomeScreen />;
 }
