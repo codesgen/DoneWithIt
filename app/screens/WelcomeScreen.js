3a9d01482ca44c23b5e24a5e90fe83eb929cca42
Abel Masila <abelmasila@gmail.com>
2021-04-09 03:13:39 +0300

    extract colors

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
 
+import colors from "./../config/colors";
+
 function WelcomeScreen(props) {
   return (
     <ImageBackground
@@ -27,12 +29,12 @@ const styles = StyleSheet.create({
   loginButton: {
     width: "100%",
     height: 70,
-    backgroundColor: "#fc5c65",
+    backgroundColor: colors.primary,
   },
   registerButton: {
     width: "100%",
     height: 70,
-    backgroundColor: "#4ecdc4",
+    backgroundColor: colors.secondary,
   },
   logoContainer: {
     position: "absolute",
