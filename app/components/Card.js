cbd0fc63381cfafafa381a1f9d65c20ed909429d
Abel Masila <abelmasila@gmail.com>
2021-04-09 04:17:09 +0300

    Added card component

---
+++ app/components/Card.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, StyleSheet, Image } from "react-native";
+
+import AppText from "./AppText";
+import colors from "./../config/colors";
+
+function Card({ title, subtitle, image }) {
+  return (
+    <View style={styles.card}>
+      <Image source={image} style={styles.image} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subtitle}>{subtitle}</AppText>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 15,
+    backgroundColor: colors.white,
+    marginBottom: 20,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  detailsContainer: {
+    padding: 20,
+  },
+  title: {
+    marginBottom: 7,
+  },
+  subtitle: {
+    color: colors.secondary,
+    fontWeight: "bold",
+  },
+});
+
+export default Card;
